eba71a4b0017eb3873f4890d022f1a3490d5e04f
yujamint <woawoa0513@gmail.com>
2024-09-04 22:07:36 +0900

    feat: 프로젝트 상세 정보 조회 API

---
+++ src/main/kotlin/com/org/framelt/project/application/port/in/ProjectDetailModel.kt
@@ -0,0 +1,22 @@
+package com.org.framelt.project.application.port.`in`
+
+import com.org.framelt.project.domain.Concept
+import com.org.framelt.project.domain.Spot
+import com.org.framelt.user.domain.LocationType
+import java.time.LocalDateTime
+
+data class ProjectDetailModel(
+    val id: Long,
+    val title: String,
+    val description: String,
+    val shootingAt: LocalDateTime,
+    val locationType: LocationType,
+    val spot: Spot,
+    val concepts: List<Concept>,
+    val conceptPhotoUrls: List<String>,
+    val retouchingDescription: String?,
+    val managerId: Long,
+    val managerNickname: String,
+    val managerProfileImageUrl: String?,
+    val managerDescription: String?,
+)
