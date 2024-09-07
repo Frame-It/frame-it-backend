6a6c24234f1928ff0a727e3a284159e232f2cfa7
yujamint <woawoa0513@gmail.com>
2024-09-07 22:44:40 +0900

    feat: 프로젝트 지원자 수락 및 스터디 시작 API

---
+++ src/main/kotlin/com/org/framelt/project/application/port/in/ProjectApplicantAcceptCommand.kt
@@ -0,0 +1,6 @@
+package com.org.framelt.project.application.port.`in`
+
+data class ProjectApplicantAcceptCommand(
+    val projectId: Long,
+    val applicantId: Long,
+)
--- src/main/kotlin/com/org/framelt/project/application/port/in/ProjectApplyUseCase.kt
+++ src/main/kotlin/com/org/framelt/project/application/port/in/ProjectApplyUseCase.kt
@@ -2,4 +2,6 @@ package com.org.framelt.project.application.port.`in`
 
 interface ProjectApplyUseCase {
     fun applyProject(projectApplyCommand: ProjectApplyCommand): ProjectApplyModel
+
+    fun acceptApplicant(projectApplicantAcceptCommand: ProjectApplicantAcceptCommand)
 }
