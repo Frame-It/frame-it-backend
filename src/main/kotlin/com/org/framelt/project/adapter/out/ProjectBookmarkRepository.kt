19272020c82aeb544ec21ea1ad41896dada22061
yujamint <woawoa0513@gmail.com>
2024-09-10 11:19:54 +0900

    feat: 프로젝트 북마크 API

---
+++ src/main/kotlin/com/org/framelt/project/adapter/out/ProjectBookmarkRepository.kt
@@ -0,0 +1,27 @@
+package com.org.framelt.project.adapter.out
+
+import com.org.framelt.project.application.port.out.ProjectBookmarkCommandPort
+import com.org.framelt.project.application.port.out.ProjectBookmarkQueryPort
+import com.org.framelt.project.domain.ProjectBookmark
+import com.org.framelt.user.adapter.out.UserJpaEntity
+import org.springframework.stereotype.Repository
+
+@Repository
+class ProjectBookmarkRepository(
+    val projectBookmarkJpaRepository: ProjectBookmarkJpaRepository,
+) : ProjectBookmarkQueryPort,
+    ProjectBookmarkCommandPort {
+    override fun existsBookmark(
+        projectId: Long,
+        userId: Long,
+    ): Boolean = projectBookmarkJpaRepository.existsByProjectIdAndUserId(projectId, userId)
+
+    override fun bookmarkProject(projectBookmark: ProjectBookmark) {
+        val projectBookmarkJpaEntity =
+            ProjectBookmarkJpaEntity(
+                project = ProjectJpaEntity.fromDomain(projectBookmark.project),
+                user = UserJpaEntity.fromDomain(projectBookmark.user),
+            )
+        projectBookmarkJpaRepository.save(projectBookmarkJpaEntity)
+    }
+}
---
+++ src/main/kotlin/com/org/framelt/project/application/port/in/ProjectBookmarkUseCase.kt
@@ -0,0 +1,5 @@
+package com.org.framelt.project.application.port.`in`
+
+interface ProjectBookmarkUseCase {
+    fun bookmarkProject(projectBookmarkCommand: ProjectBookmarkCommand)
+}
