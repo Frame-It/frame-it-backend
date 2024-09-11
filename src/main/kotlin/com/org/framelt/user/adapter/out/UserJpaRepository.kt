f0b97b1af1e867630eb8c999ebfb6231da64b900
yujamint <woawoa0513@gmail.com>
2024-09-11 21:24:53 +0900

    feat: 로그인 API 영속성 어댑터 구현

--- src/main/kotlin/com/org/framelt/user/adapter/out/UserJpaRepository.kt
+++ src/main/kotlin/com/org/framelt/user/adapter/out/UserJpaRepository.kt
@@ -3,5 +3,7 @@ package com.org.framelt.user.adapter.out
 import org.springframework.data.repository.Repository
 
 interface UserJpaRepository : Repository<UserJpaEntity, Long> {
+    fun save(user: UserJpaEntity): UserJpaEntity
+
     fun findById(id: Long): UserJpaEntity
 }
--- src/main/kotlin/com/org/framelt/user/adapter/out/UserRepository.kt
+++ src/main/kotlin/com/org/framelt/user/adapter/out/UserRepository.kt
@@ -1,3 +1,49 @@
 package com.org.framelt.user.adapter.out
 
-class UserRepository
+import com.org.framelt.user.adapter.out.oauth.OAuthProvider
+import com.org.framelt.user.application.port.out.persistence.UserCommandPort
+import com.org.framelt.user.application.port.out.persistence.UserQueryPort
+import com.org.framelt.user.domain.User
+import org.springframework.stereotype.Repository
+
+@Repository
+class UserRepository(
+    private val userJpaRepository: UserJpaRepository,
+    private val oAuthUserJpaRepository: OAuthUserJpaRepository,
+) : UserQueryPort,
+    UserCommandPort {
+    override fun save(
+        user: User,
+        provider: OAuthProvider?,
+        providerUserId: String?,
+    ): User {
+        val userEntity = userJpaRepository.save(UserJpaEntity.fromDomain(user))
+        if (provider != null && providerUserId != null) {
+            oAuthUserJpaRepository.save(
+                OAuthUserJpaEntity(
+                    provider = provider,
+                    providerUserId = providerUserId,
+                    user = userEntity,
+                ),
+            )
+        }
+        return userEntity.toDomain()
+    }
+
+    override fun readById(id: Long): User {
+        val userEntity = userJpaRepository.findById(id)
+        return userEntity.toDomain()
+    }
+
+    override fun findByProviderAndProviderUserId(
+        provider: OAuthProvider,
+        providerUserId: String,
+    ): User? {
+        val oAuthUserJpaEntity =
+            oAuthUserJpaRepository.findByProviderAndProviderUserId(
+                provider,
+                providerUserId,
+            )
+        return oAuthUserJpaEntity?.user?.toDomain()
+    }
+}
