8bdee3d47990801d1fc22bb6b452d39aa534de3a
yujamint <woawoa0513@gmail.com>
2024-09-13 17:09:42 +0900

    refactor: 로그인 테스트 컨트롤러 패키지 위치 이동

--- src/main/kotlin/com/org/framelt/user/adapter/in/LoginTestController.kt
+++ src/main/kotlin/com/org/framelt/user/adapter/in/LoginTestController.kt
@@ -1,4 +1,4 @@
-package com.org.framelt.user.application.service
+package com.org.framelt.user.adapter.`in`
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
