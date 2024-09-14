af76cc6dc7601a3954a2b661eb2f3ed139ad8d87
yujamint <woawoa0513@gmail.com>
2024-09-14 16:14:42 +0900

    feat: JWT 토큰 구현

--- src/main/kotlin/com/org/framelt/user/application/service/AuthService.kt
+++ src/main/kotlin/com/org/framelt/user/application/service/AuthService.kt
@@ -3,6 +3,7 @@ package com.org.framelt.user.application.service
 import com.org.framelt.user.adapter.out.oauth.OAuthProvider
 import com.org.framelt.user.application.port.`in`.LoginCommand
 import com.org.framelt.user.application.port.`in`.LoginUseCase
+import com.org.framelt.user.application.port.out.JwtPort
 import com.org.framelt.user.application.port.out.oauth.AuthPort
 import com.org.framelt.user.application.port.out.persistence.UserCommandPort
 import com.org.framelt.user.application.port.out.persistence.UserQueryPort
@@ -12,16 +13,17 @@ import org.springframework.stereotype.Service
 @Service
 class AuthService(
     val authPort: AuthPort,
+    val jwtPort: JwtPort,
     val userQueryPort: UserQueryPort,
     val userCommandPort: UserCommandPort,
 ) : LoginUseCase {
-    override fun login(loginCommand: LoginCommand): Long {
+    override fun login(loginCommand: LoginCommand): String {
         val providerUserId = authPort.getProviderUserId(loginCommand.provider, loginCommand.code)
         val provider = OAuthProvider.of(loginCommand.provider)
         val user =
             userQueryPort.findByProviderAndProviderUserId(provider, providerUserId)
                 ?: signUp(provider, providerUserId)
-        return user.id!!
+        return jwtPort.createToken(user.id!!.toString())
     }
 
     private fun signUp(
