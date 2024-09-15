92a0e6e400d8ac905056c1b98552a7529c3641d2
ingpyo <juninpyo726@naver.com>
2024-09-15 16:27:43 +0900

    refactor: 사용하지 않는 메소드 삭제

--- src/main/kotlin/com/org/framelt/notification/application/port/out/NotificationReadPort.kt
+++ src/main/kotlin/com/org/framelt/notification/application/port/out/NotificationReadPort.kt
@@ -3,5 +3,5 @@ package com.org.framelt.notification.application.port.out
 import com.org.framelt.notification.domain.Notification
 
 interface NotificationReadPort {
-    fun findByReceiverId(userId: Long): List<Notification>
+    fun findAllByReceiverId(userId: Long): List<Notification>
 }
