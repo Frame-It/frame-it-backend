04153f8e6fea249ce9b1bad125051af3cb592401
yujamint <woawoa0513@gmail.com>
2024-09-16 13:51:34 +0900

    feat: 진행중인 프로젝트 유무 검사 API 구현

--- src/main/kotlin/com/org/framelt/user/application/port/in/UserQuitUseCase.kt
+++ src/main/kotlin/com/org/framelt/user/application/port/in/UserQuitUseCase.kt
@@ -2,4 +2,6 @@ package com.org.framelt.user.application.port.`in`
 
 interface UserQuitUseCase {
     fun quit(userQuitCommand: UserQuitCommand)
+
+    fun hasInProgressProjects(userId: Long): Boolean
 }
