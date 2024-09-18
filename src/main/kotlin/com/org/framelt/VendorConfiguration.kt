a03f447d32d972ba402b9b8abc0b55b550ca333a
yujamint <woawoa0513@gmail.com>
2024-09-19 00:14:02 +0900

    refactor: AWS S3 자격증명 방식 변경

--- src/main/kotlin/com/org/framelt/VendorConfiguration.kt
+++ src/main/kotlin/com/org/framelt/VendorConfiguration.kt
@@ -1,17 +1,25 @@
 package com.org.framelt
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.s3.S3Client
 
 @Configuration
-class VendorConfiguration {
+class VendorConfiguration(
+    @Value("\${aws.s3.access-id}") private val accessId: String,
+    @Value("\${aws.s3.secret-key}") private val secretKey: String,
+) {
     @Bean
     fun s3Client(): S3Client {
-        return S3Client.builder()
-            .credentialsProvider(InstanceProfileCredentialsProvider.create())
+        val credentials = AwsBasicCredentials.create(accessId, secretKey)
+
+        return S3Client
+            .builder()
+            .credentialsProvider(StaticCredentialsProvider.create(credentials))
             .region(Region.AP_NORTHEAST_2)
             .build()
     }
