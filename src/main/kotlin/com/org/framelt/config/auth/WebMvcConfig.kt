cad57fa35b93de254457abaf605b5b1054b06121
yujamint <woawoa0513@gmail.com>
2024-10-05 20:24:07 +0900

    feat: CORS 설정 추가

--- src/main/kotlin/com/org/framelt/config/auth/WebMvcConfig.kt
+++ src/main/kotlin/com/org/framelt/config/auth/WebMvcConfig.kt
@@ -2,11 +2,12 @@ package com.org.framelt.config.auth
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class AuthConfig(
+class WebMvcConfig(
     val authInterceptor: AuthInterceptor,
     val memberIdArgumentResolver: MemberIdArgumentResolver,
 ) : WebMvcConfigurer {
@@ -23,5 +24,14 @@ class AuthConfig(
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(memberIdArgumentResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*")
+            .maxAge(3600)
     }
 }
