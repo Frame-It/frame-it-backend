7569a9e9d78ee15d9b38047fd8feef0d9328eb72
yujamint <woawoa0513@gmail.com>
2024-10-10 21:47:12 +0900

    feat: 토큰 만료 예외에 대한 처리 추가

--- src/main/kotlin/com/org/framelt/global/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/org/framelt/global/exception/ExceptionHandler.kt
@@ -1,5 +1,7 @@
 package com.org.framelt.global.exception
 
+import io.jsonwebtoken.ExpiredJwtException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -12,6 +14,12 @@ class ExceptionHandler(
     fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> =
         ResponseEntity
             .badRequest()
+            .body(ErrorResponse(e.message))
+
+    @ExceptionHandler(ExpiredJwtException::class)
+    fun handleExpiredJwtException(e: ExpiredJwtException): ResponseEntity<ErrorResponse> =
+        ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
             .body(ErrorResponse(e.message))
 
     @ExceptionHandler(Exception::class)
