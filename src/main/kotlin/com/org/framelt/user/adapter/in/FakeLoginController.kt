5a6fe58fdf738b94991757bf3f30f7a77d4cacad
yujamint <woawoa0513@gmail.com>
2024-10-11 15:25:11 +0900

    chore: Fake 로그인 프로필 범위 수정

--- src/main/kotlin/com/org/framelt/user/adapter/in/FakeLoginController.kt
+++ src/main/kotlin/com/org/framelt/user/adapter/in/FakeLoginController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@Profile(value = ["local"])
+@Profile(value = ["!prod"])
 @RestController
 class FakeLoginController(
     val userJpaRepository: UserJpaRepository,
