6dd14ce91d0efc12cfbf101299fe9d150d8c3cec
yujamint <woawoa0513@gmail.com>
2024-10-12 11:07:09 +0900

    feat: 프로젝트 리뷰 태그 추가

--- src/main/kotlin/com/org/framelt/project/domain/ProjectReviewTag.kt
+++ src/main/kotlin/com/org/framelt/project/domain/ProjectReviewTag.kt
@@ -3,9 +3,14 @@ package com.org.framelt.project.domain
 enum class ProjectReviewTag(
     val code: String,
 ) {
-    POSE_GOOD("RE-001"),
-    RETOUCH_GOOD("RE-002"),
+    POSE("RE-001"),
+    RETOUCH("RE-002"),
     KIND("RE-003"),
+    APPOINTMENT_TIME("RE-004"),
+    MANNER("RE-005"),
+    COMMUNICATION("RE-006"),
+    CONCEPT("RE-007"),
+    NOTHING("RE-008"),
     ;
 
     companion object {
