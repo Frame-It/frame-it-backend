bd03e2174190db087416ed9fd670bf31f2c934f7
yujamint <woawoa0513@gmail.com>
2024-10-16 16:36:25 +0900

    feat: 프로필별 에러 로깅 추가

--- src/main/kotlin/com/org/framelt/global/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/org/framelt/global/exception/ExceptionHandler.kt
@@ -1,6 +1,8 @@
 package com.org.framelt.global.exception
 
 import io.jsonwebtoken.ExpiredJwtException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,23 +10,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler(
-    val internalServerErrorMessageConverter: InternalServerErrorMessageConverter,
+    private val internalServerErrorMessageConverter: InternalServerErrorMessageConverter,
 ) {
+    private val logger: Logger = LoggerFactory.getLogger(ExceptionHandler::class.java)
+
     @ExceptionHandler(IllegalArgumentException::class)
-    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> =
-        ResponseEntity
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        logger.warn("[IllegalArgumentException]", e)
+        return ResponseEntity
             .badRequest()
             .body(ErrorResponse(e.message))
+    }
 
     @ExceptionHandler(ExpiredJwtException::class)
-    fun handleExpiredJwtException(e: ExpiredJwtException): ResponseEntity<ErrorResponse> =
-        ResponseEntity
+    fun handleExpiredJwtException(e: ExpiredJwtException): ResponseEntity<ErrorResponse> {
+        logger.warn("[ExpiredJwtException]", e)
+        return ResponseEntity
             .status(HttpStatus.FORBIDDEN)
             .body(ErrorResponse("토큰이 만료되었습니다."))
+    }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(e: Exception): ResponseEntity<ErrorResponse> =
-        ResponseEntity
+    fun handleException(e: Exception): ResponseEntity<ErrorResponse> {
+        logger.error("[Exception]", e)
+        return ResponseEntity
             .internalServerError()
             .body(ErrorResponse(internalServerErrorMessageConverter.convert(e)))
+    }
 }
