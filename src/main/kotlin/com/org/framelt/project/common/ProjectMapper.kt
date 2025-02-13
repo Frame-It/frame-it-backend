7bb1ca355b0738a26362b038ac4a3ade06c6e3af
yujamint <woawoa0513@gmail.com>
2025-02-13 21:46:15 +0900

    feat: 프로젝트 공고 조회수 반환

--- src/main/kotlin/com/org/framelt/project/common/ProjectMapper.kt
+++ src/main/kotlin/com/org/framelt/project/common/ProjectMapper.kt
@@ -123,6 +123,7 @@ class ProjectMapper {
                 isClosed = projectDetail.isClosed,
                 isHost = projectDetail.isHost,
                 timeOption = projectDetail.timeOption.name,
+                viewCount = projectDetail.viewCount,
             )
 
         fun toResponse(projectItem: ProjectAnnouncementItemModel): ProjectAnnouncementItemResponse =
